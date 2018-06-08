309e0b868744a968a659cf360ff46f715867ff57
jianhan <747931407@qq.com>
2018-06-08 16:18:39 +0800

    add 用户管理页面功能（使用饿了么的element-ui）

--- code/client/src/main.js
+++ code/client/src/main.js
@@ -3,13 +3,29 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import api from './api/api'
+import accessToken from './util/accessToken'
+import http from './util/http'
+import storage from './util/storage'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI);
+Vue.prototype.$message = ElementUI.Message;
+Vue.prototype.$confirm = ElementUI.MessageBox.confirm;
+
+Vue.prototype.$token = accessToken;
+Vue.prototype.$http = http;
+Vue.prototype.$api = api;
+Vue.prototype.$storage = storage;
+
+
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+});
+
--- code/client/src/router/index.js
+++ code/client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import UserManagement from '@/view/admin/user/UserManagement'
 
 Vue.use(Router)
 
@@ -8,8 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: UserManagement
     }
   ]
 })
